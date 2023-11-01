e7ea528897a3a6acf4be0f4a2fb92f0f5023aa9f
Yeldar Kudaibergenov <mail@yeldar.org>
2023-11-01 12:38:20 +0600

    Delete comments

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,15 +23,11 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/yeldarx/openpodcasting.com/tree/main/docs',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/yeldarx/openpodcasting.com/tree/main/docs',
         },
