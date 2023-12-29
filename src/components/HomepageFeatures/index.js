a2c746eef8a036dfe66e0a9bbcb2993abfaef182
Yeldar Kudaibergenov <mail@yeldar.org>
2023-12-29 09:48:13 +0600

    replace pictures and add additional three points

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,7 +6,7 @@ const FeatureList = [
 
   {
     title: 'Тәуелсіздік',
-    Svg: require('@site/static/img/telework-multiple-people-svgrepo-com.svg').default,
+    Svg: require('@site/static/img/undraw_couple_love_re_3fw6.svg').default,
     description: (
       <>
         Подкастиң қомақты қаражат немесе арнайы жабдықты қажет етпейді. Кез келген адам үйінде немесе офисінде отырып-ақ подкаст жасап, аудиториямен бөлісе алады.
@@ -16,20 +16,50 @@ const FeatureList = [
   
   {
     title: 'Контенттің алуандығы',
-    Svg: require('@site/static/img/supporting-business-person-svgrepo-com.svg').default,
+    Svg: require('@site/static/img/undraw_audio_conversation_re_3t38.svg').default,
     description: (
       <>
-        Подкаст деген тек қана әңгіме айту, сұхбат емес. Бұл білім беру, ойын-сауық, жаңалықтар бағдарламалары мен қатар, тақырыптарды талқылау, сұхбаттар және т.б. болуы мүмкін.
+        Подкаст деген тек қана әңгіме айту немесе сұхбат емес. Бұл білім беру, ойын-сауық, жаңалықтар, бағдарламалар мен қатар, тақырыптарды талқылау және т.б. болуы мүмкін.
       </>
     ),
   },
   
   {
-    title: 'Қол жетімділік және икемділік',
-    Svg: require('@site/static/img/family-3-generations-svgrepo-com.svg').default,
+    title: 'Қол жетімді, икемділік',
+    Svg: require('@site/static/img/undraw_podcast_audience_re_4i5q.svg').default,
     description: (
       <>
         Тыңдаушы не көрермен подкастарды өзіне ыңғайлы кез келген уақытта тұтына алады. Ал подкаст эпизодтарын алдын ала жүктеп алса - интернеттің де болуы міндетті емес.
+      </>
+    ),
+  },
+
+  {
+    title: 'Қауымдастық',
+    Svg: require('@site/static/img/undraw_social_dashboard_re_ocbd.svg').default,
+    description: (
+      <>
+      Подкаст ортақ мүддесі бар тыңдарман арасында қауымдастықтың пайда болуына ықпал етеді. Көптеген подкастардың чаты, бөлек арнасы немесе арнайы аккаунттары бар.
+      </>
+    ),
+  },
+
+  {
+    title: 'Монетизация',
+    Svg: require('@site/static/img/undraw_digital_currency_qpak.svg').default,
+    description: (
+      <>
+      Подкастар тура басқа медиалар сияқты табыс әкеле алады (демеушілік, жарнама, премиум контент, т.б.) Подкаст арқылы ақша табудың ең керемет жолы әрине Value4Value.
+      </>
+    ),
+  },
+
+  {
+    title: 'Шексіз контент',
+    Svg: require('@site/static/img/undraw_digital_nomad_re_w8uy.svg').default,
+    description: (
+      <>
+      Подкастинг терең талқылаулар, ұзақ әңгімелер мен мұқият зерттеулер жасауға өте ыңғайлы. Подкаст жасаушы дәстүрлі медианың шектеулерінен еркін.
       </>
     ),
   },
