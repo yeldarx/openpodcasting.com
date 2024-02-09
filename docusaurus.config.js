9efc91160cd2ad9e65063b2389eddfd7d8dd15ba
Yeldar Kudaibergenov <mail@yeldar.org>
2024-02-09 11:55:14 +0600

    comment onBrokenLinks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   baseUrl: '/',
   organizationName: 'yeldarx',
   projectName: 'openpodcasting.com',
-  onBrokenLinks: 'throw',
+  // onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   i18n: {
     defaultLocale: 'kk',
